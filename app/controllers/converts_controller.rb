3ea0adcbfae86c635d6b41b19c51d3288cc9a08e
Yasuhiro Usutani <y_usutani@mac.com>
2021-10-27 21:18:16 +0900

    変換作成

--- app/controllers/converts_controller.rb
+++ app/controllers/converts_controller.rb
@@ -1,5 +1,19 @@
 class ConvertsController < ApplicationController
   def index
     @converts = Convert.all
+    @convert = Convert.new
   end
+
+  def create
+    @convert = Convert.new(convert_params)
+    @convert.save
+    redirect_to converts_url
+  end
+
+  private
+    def convert_params
+      params.require(:convert).permit(:in_file)
+    rescue ActionController::ParameterMissing
+      nil
+    end
 end
